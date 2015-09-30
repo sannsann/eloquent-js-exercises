b191a464a2649b5599391dedccf4b84f624d7016
Sann Chhan <sann.c.chhan@gmail.com>
2015-09-30 13:50:42 -0700

    Add answer to Flatten exercise

--- Chapter 5/js_es_ch5_flattening_exercise.js
+++ Chapter 5/js_es_ch5_flattening_exercise.js
@@ -1,2 +1,8 @@
-// Eloquent JS Exercise - Chapter 5
+// Eloquent JS Exercise - Chapter 5 - Flatten Array
 
+var arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce(function(prevVal, currentVal) {
+  return prevVal.concat(currentVal);
+}));
+// → [1, 2, 3, 4, 5, 6]
