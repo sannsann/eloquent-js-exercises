7caa52e5f6084f986665bbb444c7663b2abff5e1
Sann Chhan <sann.c.chhan@gmail.com>
2015-10-02 13:08:22 -0700

    Finish Chapter 9 exercises

---
+++ Chapter 9/js_es_ch9_quoting_style_exercise.js
@@ -0,0 +1,5 @@
+var text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+// console.log(text.replace(/'(.*)'/g, "\"$1\""));
+console.log(text.replace(/'(.+?[\.,]+)'/g, "\"$1\""));
+// → "I'm the cook," he said, "it's my job."
