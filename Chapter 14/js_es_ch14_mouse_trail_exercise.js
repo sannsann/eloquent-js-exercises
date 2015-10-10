28058d711ad88107a23cdd2eb65d015f7505547c
Sann Chhan <sann.c.chhan@gmail.com>
2015-10-10 10:18:49 -0700

    Finish Chapter 14 exercises

---
+++ Chapter 14/js_es_ch14_mouse_trail_exercise.js
@@ -0,0 +1,29 @@
+<style>
+  .trail { /* className for the trail elements */
+    position: absolute;
+    height: 6px; width: 6px;
+    border-radius: 3px;
+    background: teal;
+  }
+  body {
+    height: 300px;
+  }
+</style>
+
+<script>
+// Your code here.
+  var dots = [];
+  for (var i = 0; i < 15; i++) {
+    var node = document.createElement('div');
+    node.className = 'trail';
+    document.body.appendChild(node);
+    dots.push(node);
+  }
+  var currentDot = 0;
+  addEventListener('mousemove', function (event) {
+    var dot = dots[currentDot];
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+    currentDot = (currentDot + 1) % dots.length;
+  })
+</script>
